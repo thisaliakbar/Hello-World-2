f498d9ccdd4e5caf5feda960b64be3d6bcdfe3d6
E31230784_MOH. ALI AKBAR RAFSANJANI <153052570+thisaliakbar@users.noreply.github.com>
2024-01-30 08:18:51 +0700

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author usER
+ */
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("Halo Dunia");
+    }
+}
